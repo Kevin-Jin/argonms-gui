3d73338c972e23c9d0641ae629021417fcf885cd
Kevin Jin <Kevin-Jin@users.noreply.github.com>
2013-02-05 00:38:14 +0000

    * Updated copyright years.

--- src/argonms/gui/tab/ConsoleTab.java
+++ src/argonms/gui/tab/ConsoleTab.java
@@ -1,6 +1,6 @@
 /*
  * ArgonMS Server Manager - a process launcher and organizer utilizing Swing.
- * Copyright (C) 2011-2012  GoldenKevin
+ * Copyright (C) 2011-2013  GoldenKevin
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
